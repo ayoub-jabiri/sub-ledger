edbda5d64bd15f1f0b771cca73afcac48081c66f
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-03-12 10:33:17 +0000

    Set up the schemas and models

---
+++ models/subscription.schema.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const subSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    billingCycle: {
+        type: String,
+        enum: ["monthly", "yearly"],
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+
+export default model("Subscription", subSchema);
